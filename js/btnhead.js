e8af511fa8ed159e78ee69b752becb5cf69128a8
duc-canh <84029160+duc-canh@users.noreply.github.com>
2021-07-10 14:40:01 +0700

    btnhom1

--- js/btnhead.js
+++ js/btnhead.js
@@ -1,60 +1,49 @@
 function header(){
-    var hd = "<div class=\"container head\">\n" +
-        "        <div class=\"intruction\">\n" +
-        "            <div class=\"head1\"><a href=\"#\">Đăng ký-Đăng nhập</a> </div>\n" +
-        "            <div class=\"head1\"><a href=\"#\">Liên hệ</a> </div>\n" +
-        "            <div class=\"head1\">Chia sẻ : </div>\n" +
-        "            <div class=\"head1\"><a href=\"#\"> <img src=\"imagegroup/as4.fbicon.png\"/></a></div>\n" +
-        "            <div class=\"head1\"><a href=\"#\"> <img src=\"imagegroup/as4.intericon.png\"/></a> </div>\n" +
-        "            <div class=\"head1\"><a href=\"#\"> <img src=\"imagegroup/as4.insicon.png\"/> </a></div>\n" +
-        "            <div class=\"head1\"><a href=\"#\"> <img src=\"imagegroup/as4.youicon.png\"/></a> </div>\n" +
+    var hd = "<div class=\"container header\">\n" +
+        "        <div class=\"head_top\">\n" +
+        "            <div class=\"col-md-3\"><a href=\"#\"><img id=\"logo\" src=\"imagegroup/logo.png\"/></a> </div>\n" +
+        "            <div class=\"col-md-3\">\n" +
+        "                <div><img class=\"top-1 \" src=\"imagegroup/hotline.gif\"></div>\n" +
+        "                <div class=\"top-1 top-2\">\n" +
+        "                    <span class=\"h-sp1\">Contact Available</br></span>\n" +
+        "                    <span class=\"h-sp2\">(+00)888.666.88 </span>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-3\">\n" +
+        "                <div ><img class=\"top-1\" src=\"imagegroup/hotline.gif\"></div>\n" +
+        "                <div class=\"top-1 top-2\">\n" +
+        "                    <span class=\"h-sp1\">Our Location</br></span>\n" +
+        "                    <span class=\"h-sp2\">New York city, US</span>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-3 bt\" id=\"logo-1\"><a id=\"h-a1\" href=\"#\"> Contact Us</a></div>\n" +
         "        </div>\n" +
-        "        <div class=\"claer\"></div>\n" +
-        "        <div class=\"logo\">\n" +
-        "            <div class=\"logo1\">\n" +
-        "                <a href=\"#\">\n" +
-        "                    <p id=\"p1\">LucKy Fruit</p>\n" +
-        "                    <p id=\"p2\">Luôn song hành cùng bạn</p>\n" +
-        "                </a>\n" +
+        "        <hr class=\"col-md-12\">\n" +
+        "        <div class=\"head-mid col-md-12\">\n" +
+        "            <div class=\"col-md-3\"></div>\n" +
+        "            <div class=\"col-md-1\"><a href=\"#\"> Home</a></div>\n" +
+        "            <div class=\"col-md-1\"><a href=\"#\"> About</a></div>\n" +
+        "            <div class=\"col-md-1\"><a href=\"#\"> Choose</a></div>\n" +
+        "            <div class=\"col-md-1\"><a href=\"#\"> Team</a></div>\n" +
+        "            <div class=\"col-md-1\"><a href=\"#\"> Pricing</a></div>\n" +
+        "            <div class=\"col-md-1\"><a href=\"#\"> Contact</a></div>\n" +
+        "            <div class=\"col-md-3\"></div>\n" +
+        "        </div>\n" +
+        "        <div class=\"head-bottom col-md-12\">\n" +
+        "            <div class=\"col-md-8\">\n" +
+        "                <div class=\"hb-d1\">Best landing page </div>\n" +
+        "                <div id=\"hb-2\">AGRICULTURE IS THE</br> SCIENCE PLANTS</div>\n" +
+        "                <div class=\"hb-d1\">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor.</div>\n" +
+        "                <div id=\"hb-3\" class=\"bt\"><a href=\"#\">Readmore</a></div>\n" +
         "            </div>\n" +
-        "            <div class=\"logo1 lo2\"><a class=\"lga\" href=\"#\"><img src=\"imagegroup/hotline.gif\"/></a>\n" +
-        "                <a class=\"lga\" href=\"#\">hotline </br>0976xxxx</a>\n" +
-        "            </div>\n" +
-        "            <div class=\"logo1 lo2\">\n" +
-        "                <a class=\"lga\" href=\"#\"><img src=\"imagegroup/doitrahang.png\"/> </a>\n" +
-        "                <a class=\"lga\" href=\"#\">đổi trả hàng</br>cho đến khi bạn hài lòng </a>\n" +
-        "\n" +
-        "            </div>\n" +
-        "            <div class=\"logo1 lo2\">\n" +
-        "                <a class=\"lga\" href=\"#\"><img src=\"imagegroup/chatluong.jpg\"/> </a>\n" +
-        "                <a class=\"lga\" href=\"#\">sản phẩm lucky fruit</br> đẳng cấp thế giới</a>\n" +
+        "            <div class=\"col-md-4\">\n" +
+        "                <img src=\"imagegroup/bg1.png\" height=\"290\" width=\"355\"/>\n" +
         "            </div>\n" +
         "        </div>\n" +
         "    </div>";
     document.getElementById("hd").innerHTML = hd;
 }
 header();
-
-function nav(){
-    var nav = "<div class=\"container head\">\n" +
-        "        <div class=\"row\">\n" +
-        "            <div class=\"claer\"></div>\n" +
-        "            <div class=\"selec\">\n" +
-        "                <div class=\"selec1\" id=\"s1\"><a href=\"#\"><img src=\"imagegroup/homeicon.png\"/> </a> </div>\n" +
-        "                <div class=\"selec1\" ><a href=\"#\">Giới thiệu</a> </div>\n" +
-        "                <div class=\"selec1\" ><a href=\"#\">sản phẩm</a></div>\n" +
-        "                <div class=\"selec1\"><a href=\"#\">tin tức</a></div>\n" +
-        "                <div class=\"selec1\"><a href=\"#\">chia sẻ bí quyết</a></div>\n" +
-        "                <div class=\"selec1\"><a href=\"#\">khuyến mại</a></div>\n" +
-        "                <div class=\"selec1\"><a href=\"#\">trái cây mùa</a></div>\n" +
-        "                <div class=\"selec1\"><a href=\"#\">đặt hàng online</a></div>\n" +
-        "            </div>\n" +
-        "            <div class=\"bg\"><a href=\"#\"><img src=\"imagegroup/traicaybg.jpg\"/></a> </div>\n" +
-        "        </div>\n" +
-        "    </div>";
-    document.getElementById("nav").innerHTML = nav;
-}
-nav();
 
 function footer(){
     var ft = "<div class=\"container foot\">\n" +
@@ -85,28 +74,11 @@ function footer(){
         "            </ul>\n" +
         "        </div>\n" +
         "        <div class=\"claer\"></div>\n" +
-        "        <div class=\"end\">\n" +
+        "        <div class=\"end col-md-12\">\n" +
         "            <p id=\"pp1\">Công ty Lucky fruit</p>\n" +
         "            <p id=\"pp2\">Địa chỉ đăng ký : số 8 Tôn Thất Thuyết,Nam Từ Liêm,Hà Nội</p>\n" +
         "        </div>\n" +
-        "        <hr>\n" +
         "    </div>";
     document.getElementById("ft").innerHTML = ft;
 }
 footer();
-
-var row = document.getElementById("row");
-var p =[
-    {
-        image : "imagegroup/vusua.jpg",
-        name : "Vú sữa Lò rèn Vĩnh Kim",
-        price : "5$/1kg",
-        buy : " mua hàng"
-    },
-];
-for(var i=0;i<p.length;i++){
-    var ct = "<a href=\"#\"><img src=\"imagegroup/vusua.jpg\"></a>\n" +
-        "                <div><a href=\"#\">Vú sữa Lò rèn Vĩnh Kim</a> </div>\n" +
-        "                <div class=\"gia\">5$/1kg</div>\n" +
-        "                <div ><a class=\"datmua\" href=\"#\"> mua hàng</a></div>";
-}
